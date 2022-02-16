cedba932034d43cb9cba3f72f804fc3a36a46424
Chris <chrishaselhorst@gmail.com>
2022-02-15 20:19:38 -0500

    Changed title

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <div className="h-screen">
       <Head>
-        <title>Create Next App</title>
+        <title>SimpleVerse</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
